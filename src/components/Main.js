c218480777eea1a5f4a98ad4ec1f5b9f7b402351
bkfwebdev <bryant.feld@gmail.com>
2023-12-05 12:32:51 -0500

    component stems added

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main>
+      {/* Your component content goes here */}
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      {/* Your navigation content goes here */}
+      <ul>
+        <li><img></img></li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
